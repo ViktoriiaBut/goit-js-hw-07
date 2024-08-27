0e8d7400455b35bdbb20e1cb127b32b77b1d57ba
ViktoriiaBut <zironka.tory@gmail.com>
2024-08-27 12:09:40 +0100

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,4 +3,11 @@ function getRandomHexColor() {
       .toString(16)
       .padStart(6, 0)}`;
   }
-  
+  const button = document.querySelector(".change-color");
+  const colorSpan = document.querySelector(".color");
+
+  button.addEventListener("click", () => {
+    const rendomColor = getRandomHexColor();
+    document.body.style.backgroundColor = rendomColor;
+    colorSpan.textContent = rendomColor;
+  });
