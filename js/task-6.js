505cd1849152bd0762e2838a901d93ab88ba84ae
ViktoriiaBut <zironka.tory@gmail.com>
2024-08-28 11:37:09 +0100

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
       .toString(16)
       .padStart(6, 0)}`;
   }
+
+  const input = document.querySelector('input[type="number"]');
+  const createBtn = document.querySelector('button[data-create]');
+  const desrtroyBtn = document.querySelector('button[data-destroy]');
+  const boxesContainer = document.getElementById('boxes');
+
+  function createBoxes(amount) {
+   boxesContainer.innerHTML = '';
+
+    let size = 30;
+
+     for (let i = 0; i < amount; i++) {
+
+    const box = document.createElement('div');
+     box.style.width = `${size}px`;
+     box.style.heigh = `${size}px`;
+     box.style.backgroundColor = getRandomHexColor();
+
+     boxesContainer.appendChild(box);
+     size += 10;
+   }
+  }
+
+  createBtn.addEventListener('click', () => {
+    const amount = Number(input.value);
+
+    if(amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      input.value = '';
+    }
+     else {
+      alert()
+     }
+  });
+
+  desrtroyBtn.addEventListener('click', () => {
+    boxesContainer.innerHTML = '';
+  });
+
+  
